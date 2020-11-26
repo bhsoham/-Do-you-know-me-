5234c9f33a4cc593bd2ad397c7cdc1e79c6b6b2a
Soham Bhattacharjee <sohambhattacharjee0711@gmail.com>
2020-11-26 10:10:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var readlineSync = require("readline-sync");
+
+var score=0;
+
+console.log("Welcome to 'Do You Know SOHAM'?");
+console.log("------------------------------")
+
+function play(question,answer){
+  var userAnswer=readlineSync.question(question);
+  if (userAnswer.toLowerCase()===answer){
+    console.log("RIGHT!");
+    score++;
+  }else {
+    console.log("WRONG!")
+  }
+  console.log("Your current score is: ",score);
+  console.log("-------------------------");
+}
+
+
+var questions=[{
+  question:"Which city do I live in? ",
+  answer:"kolkata",
+},{
+  question:"Which college am I studying in (abbreviation)? ",
+  answer:"vit",
+},{
+  question:"Am I older than 19? ",
+  answer:"no",
+}]
+
+for (var i =0;i<questions.length;i++){
+  var currentQuestion=questions[i];
+  play(currentQuestion.question,currentQuestion.answer);
+}
+
+console.log("YAY! Your final score is: ",score,"/ 3")
